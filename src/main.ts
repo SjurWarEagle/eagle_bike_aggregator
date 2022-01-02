3c9e7b544656a4242a60ac22d22e5778ad13b03c
Torsten Kunkel <git@tkunkel.de>
2022-01-02 08:51:35 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {Start} from "./start";
 
 async function bootstrap() {
-    // const port =42544;
-    //
-    // const app = await NestFactory.create(AppModule);
-    // app.enableCors({origin: '*'});
-    //
-    // await app.listen(port);
+    new Start().start();
+
+    const port =42544;
+
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({origin: '*'});
+
+    await app.listen(port);
 
 }
 
-bootstrap();
+bootstrap().then(()=>{
+});
--- src/start.ts
+++ src/start.ts
@@ -7,65 +7,69 @@ import {BikeDataAggregate} from "./services/bike-data-aggregate";
 
 config();
 
-const connectUrl = `mqtt://192.168.73.148`
-const clientId = process.env.MQTT_CLIENT_ID;
-const mqttOutputTopicBase = process.env.MQTT_OUTPUT_TOPIC_BASE;
-let bikeDataAggregated = new BikeDataAggregated();
-const serviceBikeDataAggregate = new BikeDataAggregate();
+export class Start {
+    private connectUrl = `mqtt://192.168.73.148`
+    private clientId: string = process.env.MQTT_CLIENT_ID;
+    private mqttOutputTopicBase = process.env.MQTT_OUTPUT_TOPIC_BASE;
+    private bikeDataAggregated = new BikeDataAggregated();
+    private serviceBikeDataAggregate = new BikeDataAggregate();
 
-function publish(client: MqttClient, bikeDataAggregated: BikeDataAggregated) {
-    // // publish a message to a topic
-    const message = JSON.stringify(bikeDataAggregated);
-    client.publish(mqttOutputTopicBase + 'data', message, () => {
-        // console.log("Message of ", message, " is published");
-        // client.end(); // Close the connection when published
-    });
-}
+    public publish(client: MqttClient, bikeDataAggregated: BikeDataAggregated) {
+        // // publish a message to a topic
+        const message = JSON.stringify(bikeDataAggregated);
+        client.publish(this.mqttOutputTopicBase + 'data', message, () => {
+            // console.log("Message of ", message, " is published");
+            // client.end(); // Close the connection when published
+        });
+    }
 
-async function start(): Promise<void> {
+    public async start(): Promise<void> {
 
-    const client = await connect(connectUrl, {
-        clientId,
-        clean: true,
-        connectTimeout: 4000,
-        username: process.env.MQTT_USER,
-        password: process.env.MQTT_PASSWORD,
-        reconnectPeriod: 1000,
-    })
+        const client = await connect(this.connectUrl, {
+            clientId: this.clientId,
+            clean: true,
+            connectTimeout: 4000,
+            username: process.env.MQTT_USER,
+            password: process.env.MQTT_PASSWORD,
+            reconnectPeriod: 1000,
+        })
 
-    client.on('connect', () => { // When connected
+        client.on('connect', () => { // When connected
 
-        // // subscribe to a topic
-        // client.subscribe('relay', () => {
-        //     // when a message arrives, do something with it
-        //     client.on('message', (topic, message, packet) => {
-        //         // console.log(packet);
-        //         console.log("Received '" + message + "' on '" + topic + "'");
-        //     });
-        // });
+            // // subscribe to a topic
+            // client.subscribe('relay', () => {
+            //     // when a message arrives, do something with it
+            //     client.on('message', (topic, message, packet) => {
+            //         // console.log(packet);
+            //         console.log("Received '" + message + "' on '" + topic + "'");
+            //     });
+            // });
 
-        // subscribe to a topic
-        client.subscribe('/livingroom/bike', () => {
-            client.on('message', async (topic, message, packet) => {
-                // when a message arrives, do something with it
-                // console.log(bikeDataAggregated);
-                // console.log("Received '" + message + "' on '" + topic + "'");
-                const bikeData = JSON.parse(message.toString()) as BikeData;
-                bikeDataAggregated = await serviceBikeDataAggregate.aggregate(bikeData);
-                publish(client, bikeDataAggregated);
+            // subscribe to a topic
+            client.subscribe('/livingroom/bike', () => {
+                client.on('message', async (topic, message, packet) => {
+                    // when a message arrives, do something with it
+                    // console.log(bikeDataAggregated);
+                    // console.log("Received '" + message + "' on '" + topic + "'");
+                    const bikeData = JSON.parse(message.toString()) as BikeData;
+                    this.bikeDataAggregated = await this.serviceBikeDataAggregate.aggregate(bikeData);
+                    this.publish(client, this.bikeDataAggregated);
+                });
             });
+
         });
 
-    });
 
+        client.on('message', function (topic, message) {
+            // message is Buffer
+            // console.log(JSON.parse(message.toString()) as BikeData);
+            const bikeData = JSON.parse(message.toString()) as BikeData;
+            // client.end()
+        })
 
-    client.on('message', function (topic, message) {
-        // message is Buffer
-        // console.log(JSON.parse(message.toString()) as BikeData);
-        const bikeData = JSON.parse(message.toString()) as BikeData;
-        // client.end()
-    })
+    }
+
 
 }
 
-start();
+
